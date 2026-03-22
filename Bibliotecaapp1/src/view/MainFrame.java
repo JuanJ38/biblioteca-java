a9fa1cb56847088f9194634651e417a5af3faa48
JuanJose <juanjosehorna16@gmail.com>
2026-03-21 22:08:24 -0500

    Ocultando botones para usuario

--- Bibliotecaapp1/src/view/MainFrame.java
+++ Bibliotecaapp1/src/view/MainFrame.java
@@ -5,7 +5,7 @@ import controller.BibliotecaController;
 import util.ExportarExcel;
 import java.util.ArrayList;
 import model.Libro;
-
+import java.awt.GridLayout;
 public class MainFrame extends JFrame {
 
     private BibliotecaController controller = new BibliotecaController();
@@ -20,46 +20,42 @@ public class MainFrame extends JFrame {
 
         // Panel
         JPanel panel = new JPanel();
-        panel.setLayout(null);
+        panel.setLayout(new GridLayout(0, 1, 10, 10)); 
 
         // Botón agregar libro
         JButton btnAgregarLibro = new JButton("Agregar Libro");
-        btnAgregarLibro.setBounds(100, 50, 200, 30);
+       // btnAgregarLibro.setBounds(100, 50, 200, 30);
         panel.add(btnAgregarLibro);
 
         // Botón ver libros
         JButton btnVerLibros = new JButton("Ver Libros");
-        btnVerLibros.setBounds(100, 100, 200, 30);
+        //btnVerLibros.setBounds(100, 100, 200, 30);
         panel.add(btnVerLibros);
         
      // Botón ver Usuario
         JButton btnVerUsuario = new JButton("Ver Usuario");
-        btnVerUsuario.setBounds(100, 150, 200, 30);
-        panel.add(btnVerUsuario);
-        if (rol.equals("USER")) {
-        	if (rol.equals("USER")) {
-        	    btnVerUsuario.setVisible(false);
-        	    
-        	}
+        //btnVerUsuario.setBounds(100, 150, 200, 30);
+        if (!rol.equals("USER")) {
+            panel.add(btnVerUsuario);
         }
         
     // Boton para agregar Usuario   
         JButton btnAgregarUsuario = new JButton("Agregar Usuario");
-        btnAgregarUsuario.setBounds(100, 200, 200, 30);
+        //btnAgregarUsuario.setBounds(100, 200, 200, 30);
         panel.add(btnAgregarUsuario);
         
     //Boton para prestar     
         JButton btnPrestar = new JButton("Prestar Libro");
-        btnPrestar.setBounds(100, 250, 200, 30);
+        //btnPrestar.setBounds(100, 250, 200, 30);
         panel.add(btnPrestar);
         
     // Boton salir
         JButton btnSalir = new JButton("Salir");
-        btnSalir.setBounds(100, 300, 200, 30);
+        //btnSalir.setBounds(100, 300, 200, 30);
         panel.add(btnSalir);
         
         JButton btnExcel = new JButton("Exportar Libros a Excel");
-        btnExcel.setBounds(100, 350, 200, 30);
+        //btnExcel.setBounds(100, 350, 200, 30);
         panel.add(btnExcel);
         
         
