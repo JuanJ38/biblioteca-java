e3be45da3bd36e9142b5a0980f100f5384265190
JuanJose <juanjosehorna16@gmail.com>
2026-03-25 17:18:03 -0500

    Se realiza prueba en el main

--- Bibliotecaapp1/src/app/Main.java
+++ Bibliotecaapp1/src/app/Main.java
@@ -5,6 +5,6 @@ import view.Login;
 
 public class Main {
     public static void main(String[] args) {
-        new MainFrame("ADMIN").setVisible(true);
+        new Login().setVisible(true);
     }
 }
