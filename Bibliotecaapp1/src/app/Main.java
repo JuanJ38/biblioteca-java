9e50dd355a4cd4838ceebfa7b837d7bda3329bf1
JuanJose <juanjosehorna16@gmail.com>
2026-04-09 23:42:33 -0500

    se modifica el acceso main para que sea mas robusto

--- Bibliotecaapp1/src/app/Main.java
+++ Bibliotecaapp1/src/app/Main.java
@@ -1,10 +1,19 @@
 package app;
 
-import view.MainFrame;
 import view.Login;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new Login().setVisible(true);
+        // Activar antialiasing y renderizado de fuentes mejorado
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {}
+            new Login().setVisible(true);
+        });
     }
 }
