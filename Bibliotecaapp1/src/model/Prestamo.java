667e30520efdd8ca17a6896efbf54b0108210320
JuanJose <juanjosehorna16@gmail.com>
2026-04-09 23:26:06 -0500

    agregar entidad Prestamo con atributos y getters/setters"

--- Bibliotecaapp1/src/model/Prestamo.java
+++ Bibliotecaapp1/src/model/Prestamo.java
@@ -2,51 +2,44 @@ package model;
 
 import java.util.Date;
 
-
 public class Prestamo {
 
+    private int    id;
+    private int    idLibro;
+    private int    idUsuario;
+    private Date   fechaPrestamo;
+    private Date   fechaDevolucion;
+    private String tituloLibro;
+    private String nombreUsuario;
 
-
-    private int id;           // ID del préstamo en la BD
-    private int idLibro;      // ID del libro prestado
-    private int idUsuario;    // ID del usuario que tomó el libro
-    private Date fechaPrestamo;   // Fecha del préstamo
-    private Date fechaDevolucion; // Fecha de devolución (null si no se ha devuelto)
-
-    // Constructor completo (al traer datos de la BD)
     public Prestamo(int id, int idLibro, int idUsuario, Date fechaPrestamo, Date fechaDevolucion) {
-        this.id = id;
-        this.idLibro = idLibro;
-        this.idUsuario = idUsuario;
-        this.fechaPrestamo = fechaPrestamo;
-        this.fechaDevolucion = fechaDevolucion;
+        this.id = id; this.idLibro = idLibro; this.idUsuario = idUsuario;
+        this.fechaPrestamo = fechaPrestamo; this.fechaDevolucion = fechaDevolucion;
     }
 
-    // Constructor para registrar un préstamo nuevo
     public Prestamo(int idLibro, int idUsuario) {
-        this.idLibro = idLibro;
-        this.idUsuario = idUsuario;
-        this.fechaPrestamo = new Date(); // fecha actual
+        this.idLibro = idLibro; this.idUsuario = idUsuario;
+        this.fechaPrestamo = new Date();
     }
 
-    // Getters y setters
-    public int getId() { return id; }
-    public int getIdLibro() { return idLibro; }
-    public int getIdUsuario() { return idUsuario; }
-    public Date getFechaPrestamo() { return fechaPrestamo; }
-    public Date getFechaDevolucion() { return fechaDevolucion; }
+    public int    getId()              { return id; }
+    public int    getIdLibro()         { return idLibro; }
+    public int    getIdUsuario()       { return idUsuario; }
+    public Date   getFechaPrestamo()   { return fechaPrestamo; }
+    public Date   getFechaDevolucion() { return fechaDevolucion; }
+    public String getTituloLibro()     { return tituloLibro  != null ? tituloLibro  : "ID " + idLibro; }
+    public String getNombreUsuario()   { return nombreUsuario != null ? nombreUsuario : "ID " + idUsuario; }
 
-    public void setId(int id) { this.id = id; }
-    public void setIdLibro(int idLibro) { this.idLibro = idLibro; }
-    public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
-    public void setFechaPrestamo(Date fechaPrestamo) { this.fechaPrestamo = fechaPrestamo; }
-    public void setFechaDevolucion(Date fechaDevolucion) { this.fechaDevolucion = fechaDevolucion; }
+    public void setId(int id)                       { this.id = id; }
+    public void setIdLibro(int v)                   { this.idLibro = v; }
+    public void setIdUsuario(int v)                 { this.idUsuario = v; }
+    public void setFechaPrestamo(Date d)            { this.fechaPrestamo = d; }
+    public void setFechaDevolucion(Date d)          { this.fechaDevolucion = d; }
+    public void setTituloLibro(String s)            { this.tituloLibro = s; }
+    public void setNombreUsuario(String s)          { this.nombreUsuario = s; }
 
     @Override
     public String toString() {
-        return "Prestamo [ID=" + id + ", LibroID=" + idLibro + ", UsuarioID=" + idUsuario +
-                ", Prestamo=" + fechaPrestamo + ", Devolucion=" + fechaDevolucion + "]";
+        return "Prestamo[" + id + "]";
     }
-	
-	
 }
